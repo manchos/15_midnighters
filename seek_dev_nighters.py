45adc26ee7aa2668e6dbdaf0e3bd5291ba3f0a9e
manchos <vladm.ru@gmail.com>
2018-01-04 20:56:02 +0300

    simplified functions

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,5 +1,4 @@
 import requests
-import requests_cache
 import logging
 import argparse
 import os
@@ -14,10 +13,7 @@ logging.basicConfig(level=logging.ERROR)
 def get_solution_attempts_page_json(page_number,
                                     url='http://devman.org/api/challenges/solution_attempts/'):
     try:
-        page_url = '{}?page={}'.format(url, page_number)
-        response = requests.get(page_url)
-        logging.info('The page: {} get from {}'.format(page_url,
-            'cache' if response.from_cache else 'download online'))
+        response = requests.get(url, params={'page': page_number})
         return response.json()
     except (ConnectionError, requests.exceptions.ConnectionError) as exc:
         logging.error(exc)
@@ -41,9 +37,8 @@ def load_solution_attempts(url='http://devman.org/api/challenges/solution_attemp
 
 
 def get_midnighter_name_and_attempt_time(solution_attempt, midnighters_time_seconds_duration=18000):
-    utc = pytz.utc
-    utc_dt = utc.localize(datetime.utcfromtimestamp(solution_attempt['timestamp']))
-    local_dt = utc_dt.astimezone(timezone(solution_attempt['timezone']))
+    local_dt = datetime.fromtimestamp(solution_attempt['timestamp'],
+                                      timezone(solution_attempt['timezone']))
     midnight = local_dt.replace(hour=0, minute=0, second=0)
     if 0 < (local_dt - midnight).seconds < midnighters_time_seconds_duration:
         return {'username': solution_attempt['username'], 'attempt_time': local_dt}
@@ -52,17 +47,17 @@ def get_midnighter_name_and_attempt_time(solution_attempt, midnighters_time_seco
 def get_midnighters_dict(solution_attempts):
     midnighters = defaultdict(list)
     for attempt in solution_attempts:
-        midnighter = get_midnighter_name_and_attempt_time(attempt)
-        if midnighter is not None:
-            midnighters[midnighter['username']].append(midnighter['attempt_time'].
-                                                       strftime('%d.%m.%Y %H:%M:%S %Z%z'))
+        attempt_dict = get_midnighter_name_and_attempt_time(attempt)
+        if attempt_dict is not None:
+            username, attempt_time = attempt_dict.values()
+            midnighters[username].append(attempt_time.strftime('%d.%m.%Y %H:%M:%S %Z%z'))
     return midnighters
 
 
 def print_midnighters(midnighters_dict):
-    print("\nDevman midnighters who uploaded homeworks to devman between 0am and 5am:\n")
+    print("\nDevman midnighters who uploaded homeworks to devman between 0am and 5am:")
     for midnighter_name, midnighter_attemps_list in midnighters_dict.items():
-        print(midnighter_name)
+        print('\n{}'.format(midnighter_name))
         for attempt in midnighter_attemps_list:
             print('\t{}'.format(attempt))
 
